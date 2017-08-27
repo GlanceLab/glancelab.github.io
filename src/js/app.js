d2e907ee3cca7183a96ce53f44ebc3629c964f52
Rex Low <qiweilow950823@gmail.com>
2017-08-27 18:54:57 +0800

    added footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,8 @@
 function hamburgerMenu() {
-    var x = document.getElementById("topNav");
-    console.log(x)
-    if (x.className === "header") {
-      x.className += " responsive";
-    } else {
-      x.className = "header";
-    }
+  var x = document.getElementById("topNav");
+  if (x.className === "header") {
+    x.className += " responsive";
+  } else {
+    x.className = "header";
   }
+}
