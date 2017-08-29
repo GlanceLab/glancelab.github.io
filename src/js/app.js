0297d4029eefbf7fb14f43ac6e49fcadbd806cdf
Rex Low <qiweilow950823@gmail.com>
2017-08-29 16:38:58 +0800

    remove parsing

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-function hamburgerMenu() {
-  var x = document.getElementById("topNav");
-  if (x.className === "header") {
-    x.className += " responsive";
-  } else {
-    x.className = "header";
-  }
-}
