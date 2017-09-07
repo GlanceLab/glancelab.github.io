36cea05cbcd26dea2ce6125af4d4a1d975e57de0
Rex Low <qiweilow950823@gmail.com>
2017-09-07 15:40:44 +0800

    updated sizes

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
   $(".scroll").click(function() {
     $("body").animate(
       {
-        scrollTop: eval($("#" + $(this).attr("target")).offset().top - 65)
+        scrollTop: eval($("#" + $(this).attr("target")).offset().top - 50)
       },
       750
     );
